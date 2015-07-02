76c83852e24a59471350f1850901207ff00defac
warlyware <danward@gmail.com>
2015-07-02 15:24:15 -0700

    Add question modal, question POST control

--- script.js
+++ script.js
@@ -2,5 +2,19 @@
 
 angular.module('heimdall', [])
 
-.controller('MainCtrl', function($scope){
+.controller('MainCtrl', function($scope, $http){
+  $scope.submitQuestion = function(evt) {
+    $http.post('http://localhost:3000/questions', $scope.question).success(function(data) {
+      console.log(data);
+    }).error(function(err) {
+      console.log(err);
+    });
+    $location.path('/');
+  }
+
+  $http.get('http://localhost:3000/limitquestions').success(function(data) {
+    $scope.questions = data;
+  }).error(function(err) {
+    console.log(err);
+  });
 });
